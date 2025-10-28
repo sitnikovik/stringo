91f0117fc757a9620c08906ba0533ffe071ada78
Ilya Sitnikov <sitnikovik@gmail.com>
2025-10-28 23:39:22 +0300

    feat: add numeronym funcs
    - Added `Numeronym()` converts any string (in Unicode) to its numeronym form
    - Added `NumeronymASCII()` converts only ASCII strings to its numeronym form to get more performance

---
+++ numeronym/numeronym.go
@@ -0,0 +1,50 @@
+package numeronym
+
+import "strconv"
+
+// Numeronym converts a string into its numeronym form.
+//
+// A numeronym is a word where the middle letters are replaced by the count of those letters.
+// For example, "kubernetes" becomes "k8s" because there are 8 letters between 'k' and 's'.
+// If the input string has less than 3 characters, it is returned unchanged.
+//
+// This function correctly handles Unicode strings.
+//
+// If you require better performance and are sure the input is ASCII, consider using NumeronymASCII().
+func Numeronym(s string) string {
+	rr := []rune(s)
+	n := len(rr)
+	if n < 3 {
+		return s
+	}
+	num := []rune(strconv.Itoa(n - 2))
+	res := make([]rune, len(num)+2)
+	res[0] = rr[0]
+	for i, r := range num {
+		res[i+1] = r
+	}
+	res[len(res)-1] = rr[n-1]
+	return string(res)
+}
+
+// NumeronymASCII converts an ASCII string into its numeronym form.
+//
+// A numeronym is a word where the middle letters are replaced by the count of those letters.
+// For example, "kubernetes" becomes "k8s" because there are 8 letters between 'k' and 's'.
+// If the input string has less than 3 characters, it is returned unchanged.
+//
+// This function is optimized for ASCII strings and may not handle Unicode characters correctly.
+//
+// If you need to handle Unicode strings, consider using Numeronym().
+func NumeronymASCII(s string) string {
+	n := len(s)
+	if n < 3 {
+		return s
+	}
+	num := []byte(strconv.Itoa(n - 2))
+	res := make([]byte, 0, len(num)+2)
+	res = append(res, s[0])
+	res = append(res, num...)
+	res = append(res, s[n-1])
+	return string(res)
+}
